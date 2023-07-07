19597a9f017e2843ca7c4d33c6e7081c09ba2427
Shivani verma <shivani.vr.verma@gmail.com>
2023-07-07 17:28:09 +0530

    Added Animations

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-gsap.from('#center>h2,#heading,p,#first',{
-    y:500,
-    duration:1,
+gsap.from("#nav h1,#nav-2 h6,#nav-3 i button,#nav-3 i,button",{
+    y:50,
+    duration:.5,
+})
+
+let tz = gsap.timeline()
+
+tz.from("#center h2,#center h1,#center p",{
+    y:100,
     opacity:0,
+    stagger:.25
+})
+.from("#first",{
+    y:100,
+    opacity:0,
+    stagger:.25
+})
+
+// second page-2
+let tz2 = gsap.timeline()
+tz2.from("#page-2 h2,#page-2 p",{
+    scrollTrigger:{
+        trigger:"#page-2 h2,#page-2 p",
+        scroller:"body",
+        start:"top 40%",
+        end:"top 60%",
+        // markers:true,
+        scrub:2
+    },
+    y:100,
+    opacity:0,
+    stagger:.25
+})
+.from("#inner-container",{
+    scrollTrigger:{
+        trigger:"#page-2 h2,#page-2 p",
+        scroller:"body",
+        start:"top 40%",
+        end:"top 60%",
+        // markers:true,
+        scrub:2
+    },
+    y:50,
+    opacity:0,
+    stagger:.25
 })
