907ecdbc14864f05a70d23ba3a3faf8815d7bd2a
Shivani verma <shivani.vr.verma@gmail.com>
2023-07-14 22:22:13 +0530

    create signup page,card,menu

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
+// Lococmotive code start
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#nav h1,#nav-2 h6,#nav-3 i button,#nav-3 i,button",{
-    y:30,
-    duration:.11,
-    stagger:.25,
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+
+
+
+
+// Lococmotive code end
+
+// gsap or scrollTrigger
+gsap.to("#nav h1,#nav h6,#nav h4,#nav h3,#nav #btn1,#nav #btn2",{
+    y:20,
+    duration:.1,
+    stagger:.2,
     opcity:0
 })
 
@@ -24,7 +60,7 @@ let tz2 = gsap.timeline()
 tz2.from("#page-2 h2,#page-2 p",{
     scrollTrigger:{
         trigger:"#page-2 h2,#page-2 p",
-        scroller:"body",
+        scroller:"#main",
         start:"top 40%",
         end:"top 60%",
         // markers:true,
@@ -37,7 +73,7 @@ tz2.from("#page-2 h2,#page-2 p",{
 .from("#inner-container .elem",{
     scrollTrigger:{
         trigger:"#inner-container .elem",
-        scroller:"body",
+        scroller:"#main",
         start:"top 40%",
         end:"top 60%",
         // markers:true,
@@ -53,7 +89,7 @@ let tl3 = gsap.timeline()
 scrollTrigger:{
     trigger:"#bar-menu",
     // trigger:"#bar h1,#bar p",
-    scroller:"body",
+    scroller:"#main",
     start:"top 50%",
     end:"top 80%",
     // markers:true,
@@ -72,7 +108,7 @@ tl3.from('.fill',{
     stagger:.21,
     scrollTrigger:{
         trigger:'.fill',
-        scroller:'body',
+        scroller:'#main',
         star:'top 30%',
         end:'top 80%',
         // markers:true,
@@ -84,7 +120,7 @@ tl3.from('.fill',{
 tl3.from("#page-3 h1",{
     scrollTrigger:{
     trigger:'#page-3 h1',
-    scroller:'body',
+    scroller:'#main',
     start:'top 20%',
     end:'top 30%',
     // markers:true,
@@ -98,7 +134,7 @@ tl3.from("#page-3 h1",{
 tl3.from(".card",{
     scrollTrigger:{
         trigger:'.card',
-        scroller:'body',
+        scroller:'#main',
         start:"top 30%",
         end:"top 100%",
         // markers:true,
@@ -114,12 +150,12 @@ tl3.from(".card",{
 tl3.from('#page-4 h2',{
     x:-1000,
     y:150,
-    duration:1,
+    duration:.1,
     opacity:0,
     stagger:.25,
     scrollTrigger:{
         trigger:'#page-4 h2',
-        scroller:'body',
+        scroller:'#main',
         start:"top 20%",
         end:"top 70%",
         scrub:5
@@ -135,7 +171,7 @@ tl3.from('#page-4 p',{
     color:"#FF5858",
     scrollTrigger:{
         trigger:'#page-4 p',
-        scroller:'body',
+        scroller:'#main',
         start:"top 50%",
         end:"top 70%",
         scrub:4
@@ -149,7 +185,7 @@ tl3.from('#wrapper-3 ',{
     stagger:.2,
     scrollTrigger:{
         trigger:'#wrapper-3 .card-3',
-        scroller:'body',
+        scroller:'#main',
         start:"top 50%",
         end:"top 90%",
         scrub:3,
@@ -162,10 +198,49 @@ tl3.from('#page-5',{
     opacity:0,
     scrollTrigger:{
         trigger:'#page-5',
-        scroller:'body',
+        scroller:'#main',
         // markers:true,
         start:'top 50%',
         end:'top 30%',
         scrub:4
     }
 })
+
+// card
+
+let card = document.querySelector('h3 ')
+let shop_card = document.querySelector('#shop-card')
+console.log(shop_card);
+let flag = 0
+
+card.addEventListener('click',function(e){
+    if (flag == 0) {
+        shop_card.style.display = 'initial'
+        card.innerHTML = '<i class="ri-close-line"></i>'
+        flag = 1
+    } else {
+        shop_card.style.display = 'none'
+        card.innerHTML = '<i class="ri-shopping-cart-fill"></i>'
+        flag = 0
+    }
+})
+
+// slide menu
+let menu = document.querySelector('#nav-3 h4')
+let slide = document.querySelector('#slide-menu')
+console.log(slide);
+let flag1 = 0
+
+menu.addEventListener('click',function(e){
+    if (flag1 == 0) {
+        slide.style.left = 0
+        menu.innerHTML = '<i class="ri-close-circle-fill"></i>'
+        flag1 = 1
+    } else {
+        slide.style.left = '-100%'
+        menu.innerHTML = '<i id="menu" class="ri-menu-fold-fill"></i>'
+        flag1 = 0
+    }
+})
+
+// <i class="ri-close-circle-fill"></i>
---
+++ scriptsign.js
@@ -0,0 +1,10 @@
+
+let tz = gsap.timeline()
+tz.from('#signup-center',{
+    y:100,
+    duration:1,
+    opacitya:0,
+
+    }
+)
+
